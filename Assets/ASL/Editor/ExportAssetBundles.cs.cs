fb7f215162396b1209b54e2251211a26a56fa181
TaranChristensen <TaranChristensen@gmail.com>
2017-04-29 10:07:17 -0700

    Unity 5.5.3 version of quality assurance changes and restructure

---
+++ Assets/ASL/Editor/ExportAssetBundles.cs.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/ASL/StreamingAssets/AssetBundlesPC", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        BuildPipeline.BuildAssetBundles("Assets/ASL/StreamingAssets/AssetBundlesAndroid", BuildAssetBundleOptions.None, BuildTarget.Android);
+        //BuildPipeline.BuildAssetBundles("Assets/ASL/StreamingAssets/AssetBundlesHololens", BuildAssetBundleOptions.None, BuildTarget.WSAPlayer);
+    }
+}
